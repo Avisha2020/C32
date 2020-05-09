b3a0d8a90164ff8504d1b32eca5d5446a090a123
Avisha2020 <62469067+Avisha2020@users.noreply.github.com>
2020-05-09 18:05:13 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -96,7 +96,7 @@ function keyPressed(){
     }
 }
 async function getBackgroundImage(){
-    var response=await fetch("http://worldtimeapi.org/api/timezone/Asia/kolkata");
+    var response=await fetch("https://worldtimeapi.org/api/timezone/Asia/kolkata");
     var responseJSON=await response.json();
     console.log(responseJSON);
     var dateTime= responseJSON.datetime;
